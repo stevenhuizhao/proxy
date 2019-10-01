39385a9a1d74d8ec4bae0b88834621910f989d8a
steven <tswoshikg@gmail.com>
2019-10-01 20:53:56 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/api/autocomplete/:query', (req, res) => {
+    const endpoint =`http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=c2NaQFDA4Z8RgPb7ymQi&app_code=tW2AcIJC1znWusv4LZ32SA&query=${req.params.query}&beginHighlight=<b>&endHighlight=</b>&country=AUS&maxresults=5`;
+    axios({
+        method: 'GET',
+        url : endpoint, 
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        }).then(response => {
+            res.send(JSON.stringify(response.data));
+        }).catch(error => {
+            res.send(JSON.stringify(error),400);
+        })
+    
+})
+
+app.listen(3002,()=> {
+    console.log('app is running on 3002');
+})
