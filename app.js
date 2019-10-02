03ca01bd4c0588b8a0bae525708a634d160bf6a8
steven <tswoshikg@gmail.com>
2019-10-03 00:25:36 +1000

    modify js

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const axios = require('axios');
+const router = require('./routes/router')
+require('dotenv').config()
 const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001
 
-app.get('/api/autocomplete/:query', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    const endpoint =`${process.env.ENDPOINT}?app_id=${process.env.APP_ID}&app_code=${process.env.APP_CODE}&query=${req.params.query}&beginHighlight=<b>&endHighlight=</b>&country=AUS&maxresults=5`;
-    axios({
-        method: 'GET',
-        url : endpoint, 
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-        }).then(response => {
-            res.send(JSON.stringify(response.data));
-        }).catch(error => {
-            res.send(JSON.stringify(error),400);
-        });
-});
-app.get('/ping', (req, res) => {
-    res.send('pong')
-});
-app.use('/', express.static('client'))
+app.use(router);
 
-app.listen(PORT,()=> {
-    console.log('Express server is running');
-})
+app.listen(PORT, () => console.log('Express server is running'));
