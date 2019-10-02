f8aa7cf6b5dd0a319bd5a672b3ce9184117811e0
steven <tswoshikg@gmail.com>
2019-10-02 12:45:03 +1000

    add more

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3002;
 
 app.get('/api/autocomplete/:query', (req, res) => {
-    const endpoint =`http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=c2NaQFDA4Z8RgPb7ymQi&app_code=tW2AcIJC1znWusv4LZ32SA&query=${req.params.query}&beginHighlight=<b>&endHighlight=</b>&country=AUS&maxresults=5`;
+    res.setHeader('Content-Type', 'application/json');
+    const endpoint =`${process.env.ENDPOINT}?app_id=${process.env.APP_ID}&app_code=${process.env.APP_CODE}&query=${req.params.query}&beginHighlight=<b>&endHighlight=</b>&country=AUS&maxresults=5`;
     axios({
         method: 'GET',
         url : endpoint, 
-        headers:{
+        headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
         }
@@ -17,9 +18,12 @@ app.get('/api/autocomplete/:query', (req, res) => {
             res.send(JSON.stringify(response.data));
         }).catch(error => {
             res.send(JSON.stringify(error),400);
-        })
-    
-})
+        });
+});
+app.get('/ping', (req, res) => {
+    res.send('pong')
+});
+app.use('/', express.static('client'))
 
 app.listen(PORT,()=> {
     console.log('Express server is running');
