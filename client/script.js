42d2ee56e87d39c65eeee2c7081d119f36891a8e
steven <tswoshikg@gmail.com>
2019-10-02 15:53:29 +1000

    modify

--- client/script.js
+++ client/script.js
@@ -1,59 +1,59 @@
 
-(function() {
-    document.getElementById('autocomplete-input').addEventListener('keyup',(element)=>{
-      document.getElementById('autocomplete-control').classList.add('is-loading');  
-      debounce(fireSearch,1000)(element.target.value);
-    });
-    const fireSearch = async(input) => {
-      document.getElementById('autocomplete-dropdown-items').innerHTML = '';
+(function () {
+  document.getElementById('autocomplete-input').addEventListener('keyup', (element) => {
+    document.getElementById('autocomplete-control').classList.add('is-loading');
+    debounce(fireSearch, 1000)(element.target.value);
+  });
+  const fireSearch = async (input) => {
+    document.getElementById('autocomplete-dropdown-items').innerHTML = '';
+    document.getElementById('dropdown-menu').classList.add('is-hidden');
+
+    const response = await remoterCall(input);
+    document.getElementById('autocomplete-control').classList.remove('is-loading');
+
+    if (!response.suggestions) {
       document.getElementById('dropdown-menu').classList.add('is-hidden');
-
-      const response = await remoterCall(input)
-      document.getElementById('autocomplete-control').classList.remove('is-loading');
-
-      if(!response.suggestions){
-        document.getElementById('dropdown-menu').classList.add('is-hidden');
-        return;
-      }
-      response.suggestions.forEach(element => {
-        document.getElementById('dropdown-menu').classList.remove('is-hidden');
-        document.getElementById('autocomplete-dropdown-items').insertAdjacentHTML('beforeend', `
+      return;
+    }
+    response.suggestions.forEach(element => {
+      document.getElementById('dropdown-menu').classList.remove('is-hidden');
+      document.getElementById('autocomplete-dropdown-items').insertAdjacentHTML('beforeend', `
         <a href="#" onclick="setInputValue(this)" class="dropdown-item">
             ${element.label}
         </a>
         `)
-      });
-      
-    }
+    });
 
-    async function remoterCall(input){
-        const response = await fetch(`/api/autocomplete/${input}`);
-        return await response.json();
-    }
-    var inThrottle;
-    var inDebounce;
-    const throttle = (func, limit) => {
+  }
 
-        return function() {
-          const args = arguments
-          const context = this
-          if (!inThrottle) {
-            func.apply(context, args)
-            inThrottle = true
-            setTimeout(() => inThrottle = false, limit)
-          }
-        }
+  async function remoterCall(input) {
+    const response = await fetch(`/api/autocomplete/${input}`);
+    return await response.json();
+  }
+  var inThrottle;
+  var inDebounce;
+  const throttle = (func, limit) => {
+
+    return function () {
+      const args = arguments
+      const context = this
+      if (!inThrottle) {
+        func.apply(context, args)
+        inThrottle = true
+        setTimeout(() => inThrottle = false, limit)
       }
+    }
+  }
 
-    const debounce = (func, delay) => {
-    
-        return function() {
-          const context = this
-          const args = arguments
-          clearTimeout(inDebounce)
-          inDebounce = setTimeout(() => func.apply(context, args), delay)
-        }
-      }
+  const debounce = (func, delay) => {
+
+    return function () {
+      const context = this
+      const args = arguments
+      clearTimeout(inDebounce)
+      inDebounce = setTimeout(() => func.apply(context, args), delay)
+    }
+  }
 }());
 
 const setInputValue = element => {
